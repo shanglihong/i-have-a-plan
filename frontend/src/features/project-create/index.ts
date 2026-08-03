0eb85790041cab804ac10835baf3a8456d6f5570
shanlihong <qiaoliu1996@gmail.com>
2026-08-03 10:06:21 +0800

    refactor: remove mock data exports from project-create feature index

--- frontend/src/features/project-create/index.ts
+++ frontend/src/features/project-create/index.ts
@@ -2,6 +2,5 @@ export { CreateProjectModal } from "./CreateProjectModal";
 export { CreateReadingProjectModal } from "./CreateReadingProjectModal";
 export { CreatePlanProjectModal } from "./CreatePlanProjectModal";
 export { PresetSkillSelector } from "./components/PresetSkillSelector";
-export * from "./mock";
 
 
